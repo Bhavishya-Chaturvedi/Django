138fdc86ea80b3a6f0dc3507d05217d3af276c89
Bhavishya Chaturvedi <bhavishyachaturvedi@gmail.com>
2025-02-23 20:04:54 +0530

    Added Views

--- posts/views.py
+++ posts/views.py
@@ -17,3 +17,29 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post/post_form.html'
+    success_url = reverse_lazy('posts:post_list')
+    succes_message = "Post was created successfully!"
+
+    def form_valid(sefl, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/posts_form.html'
+    success_message = "Post was updated successfully!"
+
+    def get_succes_url(self):
+      return reverse_lazy('posts:post_detail',kwargs={'pk':self.object.pk})  
+
+class PostDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Post 
+    success_url = reverse_lazy('posts:post_list')
+    success_message = "Post was deleted successfully!"
+    
